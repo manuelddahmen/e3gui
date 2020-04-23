ba2cecb1b2016f0a131650406edf4f4024698080
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-04-23 21:25:32 +0200

    Update DataModel.java

--- src/main/java/one/empty3/gui/DataModel.java
+++ src/main/java/one/empty3/gui/DataModel.java
@@ -59,7 +59,7 @@ public class DataModel implements PropertyChangeListener {
         setScene(scene);
         scene.cameraActive(new Camera());
         scene.cameraActive().calculerMatrice(null);
-    
+    }
 /*
     public DataModel(File inputEcXml) {
         if (!inputEcXml.isDirectory() && inputEcXml.exists()) {
@@ -247,7 +247,7 @@ public class DataModel implements PropertyChangeListener {
         }
         return null;
     }
-    /*
+    
             switch (localName)
             {
                 case "StructureMatrix":
