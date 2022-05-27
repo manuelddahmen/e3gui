80988b1416389ee943a095f616ec579f6d95e083
Manuel Dahmen <dathewolf@gmail.com>
2022-05-27 11:30:24 +0000

    Job automation

--- .space.kts
+++ .space.kts
@@ -4,6 +4,11 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Hello World!") {
-    container(displayName = "Say Hello", image = "hello-world")
+job("Build and publish") {
+    container(displayName = "Run publish script", image = "gradle") {
+        kotlinScript { api ->
+            api.gradle("build")
+            api.gradle("publish")
+        }
+    }
 }
